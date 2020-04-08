42f890f1a1a28a084e3b965e17044406b6584491
stefan <c0ntradicti0n>
2020-04-08 03:08:04 +0200

    set up, "parsing" requirements line by line

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='LayoutEagle',
     version='0.1',
-    packages=['layouteagle', ( open('requirements.txt').read().split("/n"))],
+    packages=['layouteagle', *( open('requirements.txt').read().splitlines())],
     license='GPLv3',
     long_description=open('README.md').read()
 )
