190b0a1ffa8f7d8d44c0b3890ae6949282a1aff2
stefan <c0ntradicti0n>
2020-04-15 16:07:19 +0200

    no paths to specify

--- layouteagle/LayoutEagle.py
+++ layouteagle/LayoutEagle.py
@@ -29,7 +29,7 @@ class LayoutEagle:
 
         self.pandas_path = pandas_path
 
-        self.model = LayoutModeler(output_dir=model_path)
+        self.model = LayoutModeler()
         self.feature_maker = FeatureMaker(
             pandas_pickle_path=".layouteagle/features.pckl",
             debug=True, parameterize=False)
