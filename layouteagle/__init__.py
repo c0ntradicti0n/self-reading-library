a99edd2ac588da02098580f813d082b4552514ae
stefan <c0ntradicti0n>
2020-04-23 15:43:08 +0200

    Replacer mojo working definitely more accurate

--- layouteagle/__init__.py
+++ layouteagle/__init__.py
@@ -1,1 +1,12 @@
-from .LayoutEagle import *
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import os
+import glob
+
+all_list = list()
+for f in glob.glob(os.path.dirname(__file__)+"/*.py"):
+    if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
+        all_list.append(os.path.basename(f)[:-3])
+
+__all__ = all_list  
