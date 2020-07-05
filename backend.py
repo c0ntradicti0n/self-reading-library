b387a31b4eb7756dc4bcb972bd74fcc7733c5694
stefan <c0ntradicti0n>
2020-07-05 15:40:05 +0200

    creating the react project from the project itself to publish the resources

--- backend.py
+++ backend.py
@@ -19,13 +19,14 @@ if __name__ ==  "__main__":
     import os
 
 
-
+    # start react app and proceed
     os.chdir("layout_viewer")
     os.popen("npm start")
     os.chdir("../")
 
     while True:
         try:
+            # start the resource server with gunicorn, that it can recompile, when changed
             subprocess.check_call(["gunicorn",  "--reload", "backend:api"], stdout=sys.stdout, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             sleep(10)
