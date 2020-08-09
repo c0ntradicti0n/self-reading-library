6dcfeab279233caae9e53ae82cdd2b6ca23a047c
stefan <c0ntradicti0n>
2020-08-09 19:13:41 +0200

    topic modelling and keyword extraction with elmo and textacy

--- Topics/Topics.py
+++ Topics/Topics.py
@@ -1,9 +1,14 @@
 import glob
+import logging
 from collections import Callable
 from typing import Dict, Tuple
 import numpy as np
 import lda
 import lda.datasets
+from gensim import models
+from gensim.corpora import Dictionary
+from gensim.models import Word2Vec, FastText
+from gensim.test.test_hdpmodel import dictionary
 
 from RestPublisher.Resource import Resource
 from RestPublisher.RestPublisher import RestPublisher
@@ -27,22 +32,51 @@ class Topics(RestPublisher, react) :
 
     def __call__(self, documents):
         if not self.topics:
-            print (list(zip(list(documents))))
-            texts, metas = list(zip(list(documents)))
+            """documents = list(documents)
+
+            print (len(list(zip(documents))))
+            html_paths_json_paths_txt_paths, metas = list(zip(*documents))
+            print (html_paths_json_paths_txt_paths)
+            html_paths, json_paths, txt_paths = list(zip(*html_paths_json_paths_txt_paths))
+            print (txt_paths)
+            """
+            texts = map(lambda x: x.split(" "),
+                        "Converting the sentences to a vector space model would transform them in such a way that looks at the words in all sentences, and then represents the words in the sentence with a number. "
+                        "2If the sentences were One-Hot encoded:".split("."))
 
             X = lda.datasets.load_reuters()
             vocab = lda.datasets.load_reuters_vocab()
             titles = lda.datasets.load_reuters_titles()
 
+            logging.warning("Building corpus")
+            dictionary = Dictionary(texts)
+            logging.warning("Performing Hierarchical Dirichlet Process, HDP is a non-parametric bayesian method (note the missing number of requested topics)")
+            model = models.HdpModel(
+                corpus=dictionary.doc2bow(map(texts, lambda x: x.split(" "))),
+                id2word=dictionary,
+                max_time=10)
+
+
+            print (model)
+            print (dir(model))
+            logging.warning("GENSIM")
+
+            w2v = Word2Vec(texts, min_count=1, size=5)
+
+            vocab = list(w2v.wv.vocab)
+
+
             # X.shape
             (395, 4258)
             X.sum()
             # 84010
-            model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-            model.fit(texts)  # model.fit_transform(X) is also available
+            print(X)
+            model = lda.LDA(n_topics=2, n_iter=1500, random_state=1)
+            model.fit_transform(X)  # model.fit_transform(X) is also available
             topic_word = model.topic_word_  # model.components_ also works
             n_top_words = 8
             topics = []
+            print(X)
             for i, topic_dist in enumerate(topic_word):
                 topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
                 topics.append(topics)
