a1601f3d1ab7838dd34b2280980a2b2b2e8ed846
finn_huckleberry <dagedieb@gmAIL.COM>
2022-05-17 16:13:07 +0200

    echo prediction args

--- python/helpers/model_tools.py
+++ python/helpers/model_tools.py
@@ -1,15 +1,17 @@
 import os
 import regex
+
 model_regex = r"(?P<shape>\d+)_(?P<f1>0,?\d*)_(?P<epoch>\d+)"
 import logging
 from pprint import pprint
+
 
 def find_best_model(model_dir):
     models = os.listdir(model_dir)
     try:
         best_model_path, scores = \
             max([(model_dir + "/" + p, next(m, regex.match(model_regex, "0_0,0_0")))
-                  for p in models if (m := regex.finditer(model_regex, p))],
+                 for p in models if (m := regex.finditer(model_regex, p))],
                 key=lambda t: t[1] and float(t[1][0].replace(",", ".")))
     except ValueError:
         logging.warning(f"No models found in {model_dir}")
@@ -19,7 +21,8 @@ def find_best_model(model_dir):
     return best_model_path, scores
 
 
-def model_in_the_loop(model_dir, collection_path, on_train, on_predict, training_rate_mode = 'ls', training_rate_file=None):
+def model_in_the_loop(model_dir, collection_path, on_train, on_predict, training_rate_mode='ls',
+                      training_rate_file=None):
     if not os.path.isdir(model_dir):
         os.makedirs(model_dir)
     if not os.path.isdir(collection_path):
@@ -36,8 +39,6 @@ def model_in_the_loop(model_dir, collection_path, on_train, on_predict, training
         if training_rate_mode == 'size':
             n_samples = os.path.getsize(training_rate_file)
 
-
-
         best_model_path, scores = find_best_model(model_dir)
         full_model_path = best_model_path
 
@@ -51,23 +52,23 @@ def model_in_the_loop(model_dir, collection_path, on_train, on_predict, training
         if loops.count(training_rate) > 2:
             raise RuntimeError("Going in circles!")
 
-
         print(f"{training_rate = }")
         if training_rate < 0.8:
             # let's train
 
             model_meta = on_train(
                 {'samples_files': samples_files,
-                'training_rate':samples_files})
+                 'training_rate': samples_files})
             pprint(model_meta)
             model_path = model_meta[0][0]
         else:
             # let's make more samples
-
+            args = {'best_model_path': best_model_path,
+                    'training_rate': training_rate}
+            print(f"prediction args = {args=}")
             try:
                 result = next(on_predict(
-                    {'best_model_path': best_model_path,
-                     'training_rate': training_rate}
+                    args
                 ))
                 pprint(result)
             except KeyboardInterrupt as e:
