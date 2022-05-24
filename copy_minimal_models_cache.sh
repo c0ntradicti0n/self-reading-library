efdceae6894ed39d2ef12831e1c72da6953b58a7
finn_huckleberry <dagedieb@gmAIL.COM>
2022-05-24 14:14:06 +0200

    less sudo and copy over minimal cache

--- copy_minimal_models_cache.sh
+++ copy_minimal_models_cache.sh
@@ -1,2 +1,2 @@
-rsync --progress -r -e "ssh -i ~/.ssh/moomoo" python/.layouteagle/text_box_models/_1782_0,8268912618438863_14 cloud@141.5.100.77:/home/deploy/LayoutEagle/python/.layouteagle/text_box_models/
+rsync --progress -r -e "ssh -i ~/.ssh/moomoo" python/.layouteagle/text_box_models/_1782_0,8268912618438863_14 cloud@141.5.100.77:/home/cloud/minimal_cache/text_box_models/
 rsync --progress -r -e "ssh -i ~/.ssh/moomoo" python/.layouteagle/elmo_difference_models/best.th cloud@141.5.100.77:/home/deploy/LayoutEagle/.layouteagle/elmo_difference_models/
