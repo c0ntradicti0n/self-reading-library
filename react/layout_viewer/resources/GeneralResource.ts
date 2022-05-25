2fd1ac5766548ef75abb34439c6b7a38b708f05c
finn_huckleberry <dagedieb@gmAIL.COM>
2022-05-25 22:41:05 +0200

    dont die at fetch errors

--- react/layout_viewer/resources/GeneralResource.ts
+++ react/layout_viewer/resources/GeneralResource.ts
@@ -1,7 +1,7 @@
 import {AppSettings} from "../config/connection";
 import {getRandomArbitrary} from "../../layout_viewer/src/util/array";
 
-export default class ServerResource <T> {
+export default class ServerResource<T> {
     fetch_allowed: Boolean
     read_allowed: Boolean
     correct_allowed: Boolean
@@ -11,14 +11,14 @@ export default class ServerResource <T> {
 
     id: string = ""
 
-    constructor (
-         route : string,
-         fetch_allowed = true,
-         read_allowed = true,
-         upload_allowed = true,
-         correct_allowed = true,
-         delete_allowed = true,
-         add_id = false) {
+    constructor(
+        route: string,
+        fetch_allowed = true,
+        read_allowed = true,
+        upload_allowed = true,
+        correct_allowed = true,
+        delete_allowed = true,
+        add_id = false) {
         this.route = route;
         this.fetch_allowed = fetch_allowed
         this.correct_allowed = fetch_allowed
@@ -46,8 +46,8 @@ export default class ServerResource <T> {
         }
     }
 
-     request = async (method : String, data = {}, callback: Function, is_file=false) => {
-      // Default options are marked with *
+    request = async (method: String, data = {}, callback: Function, is_file = false) => {
+        // Default options are marked with *
         console.log("URL", AppSettings.SAUSSAGEPOINT + this.route + this.id, callback)
 
         var fetch_init = {
@@ -56,54 +56,59 @@ export default class ServerResource <T> {
             //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
-                ...(!is_file ? {'Content-Type': 'application/json'}:{}),
+                ...(!is_file ? {'Content-Type': 'application/json'} : {}),
                 'API-Key': 'secret',
                 'Cache-Control': 'no-cache'
-              },
+            },
 
             //redirect: 'follow', // manual, *follow, error
             //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             body: !is_file ? JSON.stringify(data) : data
         }
-        if (method === "get")  {
+        if (method === "get") {
             delete fetch_init.body
         }
 
         console.log(fetch_init)
 
-        const response = await fetch(
+        try {
+            const response = await fetch(
                 // @ts-ignore
-            AppSettings.SAUSSAGEPOINT + this.route + this.id, fetch_init )
+                AppSettings.SAUSSAGEPOINT + this.route + this.id, fetch_init)
 
 
-        if (!response.ok) {
-            throw response.statusText
-        }
+            if (!response.ok) {
+                throw response.statusText
+            }
 
-        let result = null
-        try {
-            result = await response.json()
-                    console.log({result})
+            let result = null
+            try {
+                result = await response.json()
+                console.log({result})
 
+            } catch (e) {
+                console.log("Did not get a json back")
+                result = null
+            }
+
+            try {
+                return callback(result)
+            } catch (e) {
+                console.log("no callback given")
+            }
         } catch (e) {
-            console.log("Did not get a json back")
-            result = null
+            console.error(e)
         }
 
-        try {
-            return callback(result)
-        } catch (e) {
-            console.log("no callback given")
-        }
     }
 
-     fetch_one = async(id, callback: Function)  =>{
+    fetch_one = async (id, callback: Function) => {
         if (this.fetch_allowed) {
-            return this.request("post", id,  callback)
+            return this.request("post", id, callback)
         }
     }
 
-     fetch_all= async(callback)  =>{
+    fetch_all = async (callback) => {
         console.log("FETCH ALL", this.fetch_allowed, callback)
         if (this.fetch_allowed) {
             console.log("fetching")
@@ -112,15 +117,14 @@ export default class ServerResource <T> {
     }
 
 
-
     // @ts-ignore
-    ok = async (id , url = '', data = {}, callback) => {
-            if (this.read_allowed) {
-            this.request("post",   id, callback)
+    ok = async (id, url = '', data = {}, callback) => {
+        if (this.read_allowed) {
+            this.request("post", id, callback)
         }
     }
-     // @ts-ignore
-    change = async (json_path, value, callback) =>{
+    // @ts-ignore
+    change = async (json_path, value, callback) => {
         if (this.upload_allowed) {
             this.request("put", [json_path, value], callback)
         }
