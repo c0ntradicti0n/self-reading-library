1146abd031440b646cf4a995da296b95a2f0633f
Huckleberry Finn <stefan.werner@autisitici.org>
2022-07-13 16:42:56 +0200

    python 10 not working

--- python/language/text2speech/Txt2Mp3.py
+++ python/language/text2speech/Txt2Mp3.py
@@ -2,11 +2,20 @@ import os
 from pprint import pprint
 
 from ant import Ant
+from core import config
 from core.pathant.Converter import converter
 from core.pathant.PathSpec import cache_flow
 
+import torchaudio
+from speechbrain.pretrained import Tacotron2
+from speechbrain.pretrained import HIFIGAN
 
-@converter('txt', 'mp3')
+# Intialize TTS (tacotron2) and Vocoder (HiFIGAN)
+tacotron2 = Tacotron2.from_hparams(source="speechbrain/tts-tacotron2-ljspeech", savedir="tmpdir_tts")
+hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder")
+
+
+@converter('reading_order.page', 'mp3')
 class Txt2Mp3(Ant):
     def __init__(self, debug=True, *args, n=15, **kwargs):
         super().__init__(*args, cached=cache_flow.iterate, **kwargs)
@@ -14,18 +23,22 @@ class Txt2Mp3(Ant):
         self.debug = debug
 
     def __call__(self, iterator, *args, **kwargs):
-        for text, meta in iterator:
-            txt_path = meta['html_path'] + ".txt"
-            meta['txt_path'] = txt_path
-            meta['mp3_path'] = txt_path + '.mp3'
-            text = "\n\n".join(text)
-            with open(txt_path, "w") as f:
-                f.write(text)
+        for id, meta in iterator:
+            text = meta["text"]
+            path = meta["html_path"]
+            audio_path = f'{path}.{config.audio_format}'
 
-                self.logger.info(f"encoding \"{text[100:]}\" as mp3 on path {meta['mp3_path']}")
-            os.popen(f"text2wave -eval  '(nitech_us_bdl_arctic_hts)'  {meta['txt_path']} -o out.wave").read()
-            os.popen(f"lame out.wave {meta['mp3_path']}").read()
-            yield meta['mp3_path'], meta
+            # Running the TTS
+            mel_output, mel_length, alignment = tacotron2.encode_text(text)
+
+            # Running Vocoder (spectrogram-to-waveform)
+            waveforms = hifi_gan.decode_batch(mel_output)
+
+            # Save the waverform
+            torchaudio.save(audio_path, waveforms.squeeze(1), 22050)
+
+            yield
+
 
 
 if __name__ == "__main__":
