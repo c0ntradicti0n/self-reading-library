ff455a6fa2451b85e3f112a27fe577bf5adece32
c0ntradicti0n <stefan.werner@insiberia.net>
2022-11-28 01:02:50 +0100

    minor fixes with clicks and code style

--- python/language/transformer/FilterAlignText.py
+++ python/language/transformer/FilterAlignText.py
@@ -1,6 +1,7 @@
 from listalign.helpers import alignment_table
 from core.pathant.PathSpec import PathSpec
 from core.pathant.Converter import converter
+from helpers.str_tools import str_ascii
 from language.transformer.Pager import Pager, preprocess_text
 from listalign.word_pyalign import align
 
@@ -15,6 +16,8 @@ class FilterAlignText(PathSpec):
         for k, meta in gen:
             texts = meta["enumerated_texts"]
             filter_text = self.flags["filter_text"]
+            filter_text = str_ascii(filter_text)
+            texts = list(map(str_ascii, texts))
             all_tokens = preprocess_text(texts)
             filter_text_tokens = filter_text.split()
             alignment = align(all_tokens, filter_text_tokens)[0]
