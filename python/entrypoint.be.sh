ab0d77e7d9d35645540304719ca5caef5f59ee48
c0ntradicti0n <stefan.werner@insiberia.net>
2023-03-22 00:51:58 +0100

    more workers

--- python/entrypoint.be.sh
+++ python/entrypoint.be.sh
@@ -2,4 +2,4 @@ ssh-keygen -A
 /usr/sbin/sshd -D  &
 pip install pydevd-pycharm~=223.8836.34
 sleep 1
-uwsgi  --py-autoreload=1 --http 0.0.0.0:$PORT --module wsgi:application   --workers 1 --threads 6   --enable-threads --skip-atexit-teardown --max-worker-lifetime=200000--max-worker-lifetime-delta=200000 --harakiri=200000 -b 32768
+uwsgi  --py-autoreload=1 --http 0.0.0.0:$PORT --module wsgi:application   --workers 6 --threads 12   --enable-threads --skip-atexit-teardown --max-worker-lifetime=200000--max-worker-lifetime-delta=200000 --harakiri=200000 -b 32768
