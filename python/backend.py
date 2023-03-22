b77a1725744f6b68121e0fece437fc24ccce95cc
c0ntradicti0n <stefan.werner@insiberia.net>
2023-03-22 01:12:39 +0100

    be debugger timeout

--- python/backend.py
+++ python/backend.py
@@ -23,7 +23,7 @@ class ExampleMiddleware:
             resp: Response object that will be routed to
                 the on_* responder.
         """
-        timeout(connect_debugger(2345), 1)
+        timeout(lambda : connect_debugger(2345), 1)
 
     def process_resource(self, req, resp, resource, params):
         """Process the request after routing.
